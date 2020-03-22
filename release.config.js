8fa2f1addeb41d01b8c2540096d48b5d50d658fc
Jungwoo An <o1o9814@naver.com>
2020-03-22 14:38:03 +0900

    chore: Add github workflow (#17)
    * chore: Add github workflow
    
    * chore: Add semantic-release to github actions
    
    * chore: Change workflow name
    
    * chore: Add semantic-release dry-run option
    
    * chore: CHange push policy
    
    * chore: Change policy
    
    * chore: Add permistion to node_modules dir
    
    * chore: Remove upload node_modules
    
    * chore: Add @semantic-release plugins
    
    * chore: Add conventional-commit dep
    
    * chore: Add release.config.js
    
    * chore: Add restore cache
    
    * chore: Update release branch

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  branches: ['master'],
+  tagFormat: '${version}',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits'
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits'
+      }
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ]
+}
