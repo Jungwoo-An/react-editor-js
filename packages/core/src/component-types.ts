49d2387eb5e42883cf3712ecaffddae6fcde7dfb
Jungwoo An <o1o9814@naver.com>
2021-10-30 00:26:43 +0900

    feat: Change editor-js prop types

--- packages/core/src/component-types.ts
+++ packages/core/src/component-types.ts
@@ -2,7 +2,9 @@ import { JSX } from 'react'
 import { EditorConfig } from '@editorjs/editorjs'
 
 export interface Props extends EditorConfig {
-  enableReInitialize?: boolean
+  holder?: string
+  children?: JSX.Element | JSX.Element[]
+  enableReinitializeData?: boolean
 }
 
 export type ReactEditorJSComponent = (props: Props) => JSX.Element
