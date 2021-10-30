2cb0e196947c894cd0bc054d22af438887024a1e
Jungwoo An <o1o9814@naver.com>
2021-10-30 23:18:35 +0900

    chore: Change codesandbox example

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,19 +1,22 @@
 import ReactDOM from 'react-dom'
 import React, { Component } from 'react'
 
-import EditorJs from 'react-editor-js'
+import { createReactEditorJS } from 'react-editor-js'
 
 import { EDITOR_JS_TOOLS } from './constants'
+
+const ReactEditorJS = createReactEditorJS()
 
 class ReactEditor extends Component {
   render() {
     return (
-      <EditorJs
+      <ReactEditorJS
         tools={EDITOR_JS_TOOLS}
         data={{
-          time: 1556098174501,
+          time: 1635603431943,
           blocks: [
             {
+              id: 'sheNwCUP5A',
               type: 'header',
               data: {
                 text: 'Editor.js',
@@ -21,13 +24,14 @@ class ReactEditor extends Component {
               },
             },
             {
+              id: '12iM3lqzcm',
               type: 'paragraph',
               data: {
-                text:
-                  'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text.',
+                text: 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text.',
               },
             },
             {
+              id: 'fvZGuFXHmK',
               type: 'header',
               data: {
                 text: 'Key features',
@@ -35,6 +39,7 @@ class ReactEditor extends Component {
               },
             },
             {
+              id: 'xnPuiC9Z8M',
               type: 'list',
               data: {
                 style: 'unordered',
@@ -46,6 +51,7 @@ class ReactEditor extends Component {
               },
             },
             {
+              id: '-MhwnSs3Dw',
               type: 'header',
               data: {
                 text: 'What does it mean «block-styled editor»',
@@ -53,20 +59,21 @@ class ReactEditor extends Component {
               },
             },
             {
+              id: 'Ptb9oEioJn',
               type: 'paragraph',
               data: {
-                text:
-                  'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class="cdx-marker">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.',
+                text: 'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class="cdx-marker">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.',
               },
             },
             {
+              id: '-J7nt-Ksnw',
               type: 'paragraph',
               data: {
-                text:
-                  'There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.',
+                text: 'There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.',
               },
             },
             {
+              id: 'SzwhuyoFq6',
               type: 'header',
               data: {
                 text: 'What does it mean clean data output',
@@ -74,52 +81,52 @@ class ReactEditor extends Component {
               },
             },
             {
+              id: 'x_p-xddPzV',
               type: 'paragraph',
               data: {
-                text:
-                  'Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below',
+                text: 'Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below',
               },
             },
             {
+              id: '6W5e6lkub-',
               type: 'paragraph',
               data: {
-                text:
-                  'Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.',
+                text: 'Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.',
               },
             },
             {
+              id: 'eD2kuEfvgm',
               type: 'paragraph',
               data: {
-                text:
-                  'Clean data is useful to sanitize, validate and process on the backend.',
+                text: 'Clean data is useful to sanitize, validate and process on the backend.',
               },
             },
             {
+              id: 'N8bOHTfUCN',
               type: 'delimiter',
               data: {},
             },
             {
+              id: 'IpKh1dMyC6',
               type: 'paragraph',
               data: {
-                text:
-                  "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏",
+                text: "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏",
               },
             },
             {
+              id: 'FF1iyF3VwN',
               type: 'image',
               data: {
                 file: {
-                  url:
-                    'https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg',
+                  url: 'https://codex.so/public/app/img/external/codex2x.png',
                 },
                 caption: '',
-                withBorder: true,
+                withBorder: false,
                 stretched: false,
                 withBackground: false,
               },
             },
           ],
-          version: '2.12.4',
         }}
       />
     )
