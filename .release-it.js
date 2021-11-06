1981a8a44ed6970ddc1d77bffb28a2cfa9d51c36
Jungwoo An <o1o9814@naver.com>
2021-11-06 18:19:08 +0900

    chore!: Bump up version [skip ci]
    Fix release pipeline

--- .release-it.js
+++ .release-it.js
@@ -1,15 +1,13 @@
 module.exports = {
   git: {
-    tagName: 'v${version}',
+    tagName: '${version}',
     requireCleanWorkingDir: false,
     commitMessage: 'release: v${version}\n\n[skip ci]',
   },
   github: {
     release: true,
   },
-  npm: {
-    publish: true,
-  },
+  npm: false,
   plugins: {
     'release-it-yarn-workspaces': {
       skipChecks: true,
