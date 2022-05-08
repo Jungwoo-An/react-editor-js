10fde0d3b1b2275bbfb3a0ff5a89355c6e874b07
Jungwoo An <o1o9814@naver.com>
2022-05-08 20:29:07 +0900

    refactor: Add WrapperProps type

--- packages/@react-editor-js/client/src/ReactEditorJSClient.tsx
+++ packages/@react-editor-js/client/src/ReactEditorJSClient.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import {
-  Props as ReactEditorJSProps,
-  ReactEditorJS,
-} from '@react-editor-js/core'
+import { WrapperProps as Props, ReactEditorJS } from '@react-editor-js/core'
 
 import { ClientEditorCore } from './client-editor-core'
 import { EditorConfig } from '@editorjs/editorjs'
-
-export type Props = Omit<ReactEditorJSProps, 'factory'>
 
 function ReactEditorJSClient(props: Props) {
   const factory = React.useCallback(
